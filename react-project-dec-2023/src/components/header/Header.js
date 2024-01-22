203fe81a3835ead5f6ba1edd37ed7e1d3977f8bd
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-01-22 22:07:10 +0530

    commit

---
+++ react-project-dec-2023/src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function header() {
+  return (
+    <h1
+      style={{
+        backgroundColor: "blueviolet",
+        color: "white",
+        textAlign: "center",
+        margin: 0,
+      }}
+    >
+      This is My Header
+    </h1>
+  );
+}
---
+++ react-project-dec-2023/src/components/main/Main.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Main() {
+  return <div style={{ "min-height": "400px" }}>This is component</div>;
+}

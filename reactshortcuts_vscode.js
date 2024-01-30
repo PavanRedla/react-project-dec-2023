260fa9479b8f6fcd82f59b768e9e49eb9666e3df
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-01-30 09:08:09 +0530

    new

---
+++ reactshortcuts_vscode.js
@@ -0,0 +1,151 @@
+// IMR - import React from 'react';
+
+import React from 'react'
+
+// IMRD - import ReactDOM from 'react-dom'
+
+import ReactDOM from 'react-dom'
+
+// IMRC - import React, { Component } from 'react'
+
+import React, { Component } from 'react'
+
+// IMPT -	import PropTypes from 'prop-types'
+
+import PropTypes from 'prop-types'
+
+// RCC - React class component
+
+import React, { Component } from 'react'
+
+export default class reactshortcuts_vscode extends Component {
+  render() {
+    return (
+      <div>reactshortcuts_vscode</div>
+    )
+  }
+}
+
+// RCE - React class Export component
+
+import React, { Component } from 'react'
+
+export class reactshortcuts_vscode extends Component {
+  render() {
+    return (
+      <div>reactshortcuts_vscode</div>
+    )
+  }
+}
+
+export default reactshortcuts_vscode
+
+// RFC - React Functional Component
+
+import React from 'react'
+
+export default function reactshortcuts_vscode() {
+  return (
+    <div>reactshortcuts_vscode</div>
+  )
+}
+
+// RFCE - React Functional Export Component
+
+import React from 'react'
+
+function reactshortcuts_vscode() {
+  return (
+    <div>reactshortcuts_vscode</div>
+  )
+}
+
+export default reactshortcuts_vscode
+
+
+// RMC - React Function Memo component
+
+import React, { memo } from 'react'
+
+const reactshortcuts_vscode = memo(() => {
+  return (
+    <div>reactshortcuts_vscode</div>
+  )
+})
+
+export default reactshortcuts_vscode
+
+// RCONST - constructor with super
+
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
+
+// RPC - React Class Pure Component
+
+import React, { PureComponent } from 'react'
+
+export default class reactshortcuts_vscode extends PureComponent {
+  render() {
+    return (
+      <div>reactshortcuts_vscode</div>
+    )
+  }
+}
+
+// RPCE - React Class Pure Export Component
+
+import React, { PureComponent } from 'react'
+
+export class reactshortcuts_vscode extends PureComponent {
+  render() {
+    return (
+      <div>reactshortcuts_vscode</div>
+    )
+  }
+}
+
+export default reactshortcuts_vscode
+
+// RAFC - React Arrow Function Component
+
+import React from 'react'
+
+export const reactshortcuts_vscode = () => {
+  return (
+    <div>reactshortcuts_vscode</div>
+  )
+}
+
+
+// RAFCE - React Arrow Function export Component
+
+import React from 'react'
+
+const reactshortcuts_vscode = () => {
+  return (
+    <div>reactshortcuts_vscode</div>
+  )
+}
+
+export default reactshortcuts_vscode
+
+// REN - render() { return( ) }
+
+render(){
+    return()
+}
+
+// SST - this.setState({ })
+
+this.setState({first})
+
+
+
+
+
+

be75fc9ba6e5f4b9549715068e71481d7bcb69ca
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-01-31 22:01:09 +0530

    new

--- react-project-dec-2023/src/App.js
+++ react-project-dec-2023/src/App.js
@@ -1,20 +1,19 @@
-import React from "react";
-import Header from "./components/header/Header";
-import Main from "./components/main/Main";
-import Footer from "./components/footer/Footer";
-import Navbar from "./components/navbar/Navbar";
-import Categories from "./components/categories/Categories";
-import Carousel from "./components/carousel/Carousel";
+import React from 'react';
+import Header from './components/header/Header'
+import Main from './components/main/Main'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar';
+import Categories from './components/categories/Categories';
+import Carousel from './components/carousel/Carousel';
 
 export default function App() {
-  return (
-    <div>
-      <Header />
-      <Navbar />
-      <Categories />
-      <Carousel />
-      <Main />
-      <Footer />
-    </div>
-  );
+  console.log('I am App Component')
+  return <div>
+    {/* <Header /> */}
+    <Navbar />
+    {/* <Categories/> */}
+    {/* <Carousel /> */}
+    <Main />
+    <Footer />
+  </div>
 }

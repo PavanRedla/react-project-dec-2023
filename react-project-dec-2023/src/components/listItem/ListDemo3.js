71226f1a0a1b9759607507032fcb5f4b6396f000
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-02-01 21:59:13 +0530

    new

--- react-project-dec-2023/src/components/listItem/ListDemo3.js
+++ react-project-dec-2023/src/components/listItem/ListDemo3.js
@@ -1,29 +1,25 @@
-import React from "react";
-import products from "./products.json";
+import React from 'react';
+import products from './products.json'
 
 export default function ListDemo3() {
-  return (
-    <>
-      <h2 className="text-center">Product List</h2>
+    return <>
+        <h2 className='text-center'>Product List</h2>
 
-      <div className="container">
-        <div className="row">
-          {products.map((product) => {
-            return (
-              <div className="col-sm-3" key={product.id}>
-                <div className="card" style={{ height: "750px" }}>
-                  <img src={product.image} className="card-img-top" alt="..." />
-                  <div className="card-body text-center">
-                    <h5 className="card-title">{product.category}</h5>
-                    <p className="card-text">{product.description}</p>
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+        <div className='container'>
+            <div className='row'>
+                {products.map(product => {
+                    return <div className='col-sm-3 mb-1' key={product.id}>
+                        <div className="card">
+                            <img src={product.image} className="card-img-top" style={{ height: '200px' }} alt='something' />
+                            <div className="card-body text-center" >
+                                <h5 className="card-title">{product.category}</h5>
+                                <p className="card-text text-truncate">{product.description}</p>
+                                <button className='btn btn-primary'>Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                })}
+            </div>
         </div>
-      </div>
     </>
-  );
 }

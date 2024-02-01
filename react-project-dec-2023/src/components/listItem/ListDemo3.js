371b50945266dde64f93da22c9d6057d19f57d83
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-02-01 20:14:48 +0530

    new

--- react-project-dec-2023/src/components/listItem/ListDemo3.js
+++ react-project-dec-2023/src/components/listItem/ListDemo3.js
@@ -1,5 +1,6 @@
 import React from "react";
 import products from "./products.json";
+import "./listDemo3.css";
 
 export default function ListDemo3() {
   return (
@@ -10,14 +11,12 @@ export default function ListDemo3() {
         <div className="row">
           {products.map((product) => {
             return (
-              <div className="col-sm-3">
-                <div classname="card">
+              <div className="col-sm-3" key={product.id}>
+                <div className="card" style={{ height: "750px" }}>
                   <img src={product.image} className="card-img-top" alt="..." />
-                  <div classname="card-body text-center">
-                    <h5 classname="card-title">{product.category}</h5>
-                    <p classname="card-text" style={{ height: "150px" }}>
-                      {product.description}
-                    </p>
+                  <div className="card-body text-center">
+                    <h5 className="card-title">{product.category}</h5>
+                    <p className="card-text">{product.description}</p>
                     <button className="btn btn-primary">Buy Now</button>
                   </div>
                 </div>

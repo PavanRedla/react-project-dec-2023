b4fa8c3489585867ec6dde7fe892f1405f997dbc
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-02-04 19:37:31 +0530

    new

--- react-project-dec-2023/src/components/categories/Category.js
+++ react-project-dec-2023/src/components/categories/Category.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './categories.css'
+import React from "react";
+import "./categories.css";
 
 export default function Category(props) {
-    return <>
-        <div className='col'>
-            <div className='category text-center'>
-                <img src={props.img} alt='some text' />
-                <div>{props.name}</div>
-            </div>
+  return (
+    <>
+      <div className="col">
+        <div className="category text-center">
+          <img src={props.img} alt="some text" />
+          <div>{props.name}</div>
         </div>
+      </div>
     </>
+  );
 }

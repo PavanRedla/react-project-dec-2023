a5d41854cba0b55fe2d07c93c2bb33202730c033
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-02-13 22:11:57 +0530

    new

---
+++ react-project-dec-2023/src/components/EmployeeCRUD/EmployeeCrud.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function EmployeeCrud() {
+  return (
+    <>
+      <h2 className="text-center">THis is EmployeeCRUD Component</h2>
+      <div className="container"></div>
+    </>
+  );
+}

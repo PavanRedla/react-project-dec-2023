b688bf30632dfdbfacf6b696e2c16ce72511e635
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-02-14 20:37:22 +0530

    new

--- react-project-dec-2023/src/components/EmployeeCRUD/EmployeeList.js
+++ react-project-dec-2023/src/components/EmployeeCRUD/EmployeeList.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 
 export default function EmployeeList({
   employees,
   deleteEmployee,
   editEmployee,
 }) {
+  let [modalIsOpen, setIsOpen] = useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <>
       <table className="table table-bordered">
@@ -16,7 +24,18 @@ export default function EmployeeList({
                 <td>{emp.name}</td>
                 <td>{emp.sal}</td>
                 <td>
-                  <button className="btn btn-success">View</button>
+                  <button className="btn btn-success" onClick={openModal}>
+                    View
+                  </button>
+                  <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Example Modal"
+                  >
+                    <p>{emp.eId}</p>
+                    <button onClick={closeModal}>close</button>
+                    <div>I am a modal</div>
+                  </Modal>
                   <button
                     className="btn btn-primary mx-1"
                     onClick={() => editEmployee(emp)}

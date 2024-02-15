d61dc11230fd876e1224a358207ec15b944b214d
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-02-15 20:27:34 +0530

    new

--- react-project-dec-2023/src/components/EmployeeCRUD/AddEmployee.js
+++ react-project-dec-2023/src/components/EmployeeCRUD/AddEmployee.js
@@ -3,9 +3,30 @@ import React, { useState } from "react";
 export default function AddEmployee({ addEmployee }) {
   const initialEmp = { eId: "", name: "", sal: 0 };
   const [emp, setEmp] = useState(initialEmp);
+
+  // let person = {
+  //   name: 'David',
+  //   age: 15,
+  //   job: 'Programmer'
+  // }
+
+  // const { name, age } = person; // Takes the property/method from the object
+
+  // console.log(name); // Prints 'David'
+  // console.log(age); // Prints '15'
+  // Without using destructuring, I would have done this:
+
+  // const name = person.name;
+  // const age = person.age;
+
   const changeHandler = (e) => {
+    // const name = e.target.name;
+    // const value = e.target.value;
+    // e.target represents the input box.
+    // the above two lines can be written with destructuring as
+
     const { name, value } = e.target;
-    setEmp({ ...emp, [name]: value });
+    setEmp({ ...emp, [name]: value }); // [name]:value==> [e.target.name]:e.target.value==> eId/name/sal : value entered in i/p box
   };
   const addMyEmployee = (emp) => {
     addEmployee(emp);

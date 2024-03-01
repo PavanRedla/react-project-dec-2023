c79b56f92a105bb4d40a37f558a28d96ae04c555
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-03-01 20:49:27 +0530

    Updates

--- react-project-dec-2023/src/components/Routing/NotFound.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function NotFound() {
-  return <h2>Sorry, Page not found</h2>;
-}
--- react-project-dec-2023/src/components/memo/ComponentC.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function ComponentC({ c }) {
-  console.log("C Component rendered...");
-  return <div>ComponentC - c value is {c}</div>;
-}
-export default React.memo(ComponentC);
-
-// ComponentC is memo component hence it only gets rerendered if state variable {c} passed to it gets modified in parent component (MemoParent.js)
